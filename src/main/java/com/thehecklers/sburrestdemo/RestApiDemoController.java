317e9cae9695ab3d8162cf5d0e0eaaa478f25db2
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2023-10-28 18:26:11 -0300

    Initial commit

---
+++ src/main/java/com/thehecklers/sburrestdemo/RestApiDemoController.java
@@ -0,0 +1,73 @@
+package main.java.com.thehecklers.sburrestdemo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+class RestApiDemoController
+{ 
+    private List<Coffee> coffees = new ArrayList<>();
+
+    public RestApiDemoController()
+    { 
+        coffees.addAll(
+        List.of( 
+            new Coffee("Café Cereza"),
+            new Coffee("Café Ganador"),
+            new Coffee("Café Lareño"),
+            new Coffee("Café Três Pontas")
+        ));
+    }
+
+    /*@RequestMapping(value = "/coffes", method = RequestMethod.GET)
+    Iterable<Coffee> getCoffees() { 
+        return coffee;
+    }*/
+
+    @GetMapping("/coffees")
+    Iterable<Coffee> getCoffees() { 
+        return coffees;
+    }
+
+    @GetMapping("/coffees/{id}")
+    Optional<Coffee> getCoffeeById(@PathVariable String id) { 
+        for (Coffee c : coffees) 
+        { 
+            if (c.getId().equals(id))
+            { 
+                return Optional.of(c);
+            }
+        }
+
+        return Optional.empty();
+    }
+
+    @PostMapping("/coffees")
+    Coffee postCoffee(@RequestBody Coffee coffee) { 
+        coffees.add(coffee);
+        return coffee;
+    }
+
+    @PutMapping("/coffee/{id}")
+    Coffee putCoffee(@PathVariable String id, @RequestBody Coffee coffee) {
+        int coffeeIndex = -1; 
+
+        for (Coffee c : coffees) { 
+            if (c.getId().equals(id)) {
+                coffeeIndex = coffees.indexOf(c); coffees.set(coffeeIndex, coffee);
+            }
+        }
+
+        return (coffeeIndex == -1) ? postCoffee(coffee) : coffee;
+    }
+
+    @DeleteMapping("/coffees/{id}") void
+    deleteCoffee(@PathVariable String id) {
+        coffees.removeIf(c -> c.getId().equals(id));
+    }       
+
+}
